8757b9f1cd369ba14cb768b2999f31bb4629a3de
Aaron Muir Hamilton <aaron@correspondwith.me>
2025-04-29 13:20:10 -0400

    u32 for `PointerButtons`, use bit representation for `PointerButton` (#19)
    32 buttons is a safe upper limit on useful pointer buttons from HID
    devices, and is the maximum number of pointer buttons supported on
    Windows, which is the primary platform for exotic mice.

--- ui-events/src/pointer/mod.rs
+++ ui-events/src/pointer/mod.rs
@@ -26,11 +26,3 @@ pub enum PointerType {
     /// A touch contact.
     Touch,
 }
-
-#[cfg(test)]
-mod tests {
-    // CI will fail unless cargo nextest can execute at least one test per workspace.
-    // Delete this dummy test once we have an actual real test.
-    #[test]
-    fn dummy_test_until_we_have_a_real_test() {}
-}
